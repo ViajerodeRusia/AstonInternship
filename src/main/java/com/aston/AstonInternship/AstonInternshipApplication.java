8296c9770152302a84dc7e31ae29bc19cb68f67b
Denis Siliukov <abhem21rus@mail.ru>
2024-11-12 16:12:50 +0300

    Init

---
+++ src/main/java/com/aston/AstonInternship/AstonInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.aston.AstonInternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonInternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aston/AstonInternship/AstonInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aston.AstonInternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
